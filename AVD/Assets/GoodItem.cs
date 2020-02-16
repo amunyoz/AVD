8b83ec5ffd5944b85e9769cd5b9625184599758e
Adolfo Muñoz <amunyoz@upv.edu.es>
2020-02-16 13:17:08 +0100

    Some changes to pick items in the 2danimation scene. Script added to load scene in the Menu scene.
    Some changes to pick items in the 2danimation control scene.
    Added a script in the Menu scene to load the next scene.

--- AVD/Assets/GoodItem.cs
+++ AVD/Assets/GoodItem.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class GoodItem : MonoBehaviour
 {
+    public enum ItemType { Cherry=0,Gem=1, Frog=2}
+    public ItemType Type;
+    private void Start()
+    {
+        GetComponent<Animator>().SetInteger("Type",(int)Type);
+    }
 
     public void PickItem() {
-
-        GetComponent<Animator>().SetTrigger("Pick");
-
-    
+        GetComponent<Animator>().SetTrigger("Pick");   
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) PickItem();
     }
 }
+    
---
+++ AVD/Assets/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadSceneNumber(int level)
+    {
+        SceneManager.LoadSceneAsync(level);
+    }
+}
