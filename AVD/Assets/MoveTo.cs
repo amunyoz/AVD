6bc6e01ab1580ce34b44009900fe231e016093ec
Adolfo Muñoz <amunyoz@upv.edu.es>
2020-03-25 16:38:28 +0100

    Disco Animation with Character Navmesh and Cinemachine
    First view to cinemachine and triggering animation of a Discoteque with he Character

---
+++ AVD/Assets/MoveTo.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+public class MoveTo : MonoBehaviour
+{
+    RaycastHit hitInfo = new RaycastHit();
+    NavMeshAgent agent;
+ //  public Transform goal;// if it exist then goes to target, otherwise if waits for a click of the mouse on the area of the Nav Agent
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    public Transform Flag;
+    void Update()
+    {
+            //if (goal == null) { 
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+                        agent.destination = hitInfo.point;
+                         Flag.position = hitInfo.point;
+                         Flag.gameObject.GetComponent<Animator>().SetBool("active", true);
+        }
+           // }
+            //else {
+            //    agent.destination = goal.transform.position;
+           // }
+        }
+}
---
+++ AVD/Assets/SimpleGo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+public class SimpleGo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NavMeshAgent agent;
+    public Transform Destination;
+    Vector3 PreviousPosition;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = Destination.transform.position;
+    }
+    private void Update()
+    {
+        if (Destination.transform.position != PreviousPosition) {
+            agent.destination = Destination.transform.position;
+            PreviousPosition = Destination.transform.position;
+        }
+        
+    }
+}
+
+
