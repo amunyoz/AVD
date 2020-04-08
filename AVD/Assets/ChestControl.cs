707cab11495022c3f3d650f52fcd3b505272fdb7
Adolfo Muñoz <amunyoz@upv.edu.es>
2020-04-08 14:42:07 +0200

    Opening chest with cinemachine animation
    Opening chest with cinemachine animation and character animation.

---
+++ AVD/Assets/ChestControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(PlayableDirector))]
+[RequireComponent(typeof(Collider))]
+public class ChestControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public void Play()
+    {
+ //we play the director of the chest to orbit and open the treasure
+            GetComponent<PlayableDirector>().Play();
+   
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+             Play();
+        }
+    }
+}
